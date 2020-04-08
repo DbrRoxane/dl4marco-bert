3e7d739d6c7071b56541fe191a2bb1cc6953d4c6
Roxane Debruyker <roxane.debruyker.1@ulaval.ca>
2020-04-08 06:06:48 -0400

    Change chunk_narrativeqa_stories to store all the books in one file instead of one per question

--- chunk_narrativeqa_stories.py
+++ chunk_narrativeqa_stories.py
@@ -5,31 +5,53 @@ import re
 def chunk_story(story_str, chunk_size):
     return textwrap.wrap(re.sub('\s+', ' ',story_str), chunk_size)
 
-def chunk_story_paragraphs(story_str):
-    paragraphs = [re.sub('\s+', ' ',paragraph) for paragraph in story_str.split("\n\n")]
-    return paragraphs
+def chunk_story_paragraphs(story_id, data_dir):
+    story_file = NQA_DIR + "tmp/" + story_id + ".content"
+    with open(story_file, 'r', encoding="utf-8", errors="ignore") as f:
+        story_str = f.read()
+        chunks =  [re.sub('\s+', ' ',paragraph)
+                   for paragraph in story_str.split("\n\n")]
+    return chunks
 
-if __name__=="__main__":
-    NQA_DIR = "./data/narrativeqa/"
-    CHUNK_SIZE = 2000
-    with open(NQA_DIR+"qaps.csv", "r") as qa_file:
+def is_book(data_dir, story_id):
+    with open(data_dir+"documents.csv", "r") as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        book = [row[2]=="gutenberg" for row in csv_reader if row[0]==story_id][0]
+    return book
+
+def extract_data_entries(data_dir):
+    entries = list()
+    with open(data_dir+"qaps.csv", "r") as qa_file:
         csv_reader = csv.reader(qa_file, delimiter=',')
         next(csv_reader)
+        stories_chunked = dict()
         for question_idx, question_set in enumerate(csv_reader):
             story_id = question_set[0]
             question_str = question_set[2]
             answer_1, answer_2 = question_set[3], question_set[4]
-            with open("./data/narrativeqa/documents.csv", "r") as doc_file:
-                csv_reader_doc = csv.reader(doc_file, delimiter=',')
-                livre = [row[2]=="gutenberg" for row in csv_reader_doc if row[0]==story_id][0]
-            if livre:
-                story_file = NQA_DIR + "tmp/" + story_id
-                with open(story_file + ".content", "r" , encoding="utf-8", errors="ignore") as story_str:
-                    chunks = chunk_story_paragraphs(story_str.read())
-                with open(story_file + "_" + str(question_idx) + "_book.eval", "w") as writer:
-                    for cnt, chunk in enumerate(chunks):
-                        line = "{1}_q{0} \t {1}_p{4} \t {2} \t ({4}) - {3} \t {5} \t {6} \n".\
-                                format(question_idx, story_id, question_str, chunk, cnt, answer_1, answer_2)
-                        writer.write(line)
+            book = is_book(data_dir, story_id)
+            chunks = stories_chunked.get(story_id, list())
+            if not chunks and book:
+                chunks = chunk_story_paragraphs(story_id, data_dir)
+                stories_chunked[story_id] = chunks
+            for cnt, chunk in enumerate(chunks):
+                entries.append({
+                    'question_id':"{}_q{}".format(story_id,question_idx),
+                    'passage_id':"{}_p{}".format(story_id,cnt),
+                    'question':question_str,
+                    'passage':"({}) - {}".format(cnt, chunk),
+                    'answer1':answer_1,
+                    'answer2':answer_2})
+    return entries
 
+if __name__=="__main__":
+    NQA_DIR = "./data/narrativeqa/"
+    #CHUNK_SIZE = 2000
+    print("Start processing data")
+    entries = extract_data_entries(NQA_DIR)
+    fieldnames = entries[0].keys()
+    print("Finished processing. Now write data in {}narrative_qa_book.eval".format(NQA_DIR))
+    with open(NQA_DIR+"narrativeqa_book.eval", "w", newline='') as writer:
+        dict_writer = csv.DictWriter(writer, fieldnames=fieldnames, delimiter='\t')
+        dict_writer.writerows(entries)
 
