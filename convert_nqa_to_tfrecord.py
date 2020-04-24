a16800aad3952b2e0c86b5f2985130985e62417e
DbrRoxane <roxane.debruyker.1@ulaval.ca>
2020-04-24 11:09:47 -0400

    split tf dataset to be able to run it on CoLab

--- convert_nqa_to_tfrecord.py
+++ convert_nqa_to_tfrecord.py
@@ -17,7 +17,7 @@ FLAGS = flags.FLAGS
 
 
 flags.DEFINE_string(
-    "output_folder", "./data/narrativeqa/nqa_tf_with_answer_24avril",
+    "output_folder", "./data/narrativeqa/nqa_tf_without_answer_24avril",
     "Folder where the tfrecord files will be written.")
 
 flags.DEFINE_string(
@@ -63,6 +63,10 @@ flags.DEFINE_integer(
     "num_eval_docs", 500,
     "The maximum number of docs per query for dev and eval sets.")
 
+flags.DEFINE_integer(
+    "num_examples_per_tf", 1000000,
+    "The number of examples, which can be divided by 500, to split")
+
 
 def write_to_tf_record(writer, tokenizer, query, docs, labels,
                        ids_file=None, query_id=None, doc_ids=None):
@@ -99,7 +103,7 @@ def write_to_tf_record(writer, tokenizer, query, docs, labels,
     if ids_file:
      ids_file.write('\t'.join([query_id, doc_ids[i]]) + '\n')
 
-def convert_eval_dataset(set_name, tokenizer, use_answer=False):
+def convert_eval_dataset(set_name, tokenizer, use_answer):
   print('Converting {} set to tfrecord...'.format(set_name))
   start_time = time.time()
 
@@ -113,10 +117,18 @@ def convert_eval_dataset(set_name, tokenizer, use_answer=False):
   else:
     dataset_path = FLAGS.eval_dataset_path
 
-  queries_docs = collections.defaultdict(list)  
+  split_dataset = []
+  queries_docs = collections.defaultdict(list)
   query_ids = {}
   with open(dataset_path, 'r') as f:
     for i, line in enumerate(f):
+      if i%FLAGS.num_examples_per_tf == 0 and i > 0:
+        processed_dataset = queries_docs.copy()
+        queries_docs = collections.defaultdict(list)
+        queries_docs[query] = processed_dataset[query].copy()
+        del processed_dataset[query]
+        split_dataset.append(processed_dataset)
+        print("{} checkpoint, save {} queries ".format(i, len(processed_dataset.items())))
       query_id, doc_id, query, doc, a1, a2 = line.strip().split('\t')
       if use_answer:
         query += " "+a1 +" "+ a2
@@ -127,46 +139,46 @@ def convert_eval_dataset(set_name, tokenizer, use_answer=False):
       queries_docs[query].append((doc_id, doc, label))
       query_ids[query] = query_id
 
+  for j, queries_docs in enumerate(split_dataset):
   # Add fake paragraphs to the queries that have less than FLAGS.num_eval_docs.
-  queries = list(queries_docs.keys())  # Need to copy keys before iterating.
-  for query in queries:
-    docs = queries_docs[query]
-    docs += max(
+    queries = list(queries_docs.keys())  # Need to copy keys before iterating.
+    for query in queries:
+      docs = queries_docs[query]
+      docs += max(
         0, FLAGS.num_eval_docs - len(docs)) * [('00000000', 'FAKE DOCUMENT', 0)]
-    queries_docs[query] = docs[:FLAGS.num_eval_docs]
+      queries_docs[query] = docs[:FLAGS.num_eval_docs]
+    assert len(
+        set(len(docs) == FLAGS.num_eval_docs for docs in queries_docs.values())) == 1, (
+            'Not all queries have {} docs'.format(FLAGS.num_eval_docs))
 
-  assert len(
-      set(len(docs) == FLAGS.num_eval_docs for docs in queries_docs.values())) == 1, (
-          'Not all queries have {} docs'.format(FLAGS.num_eval_docs))
+    writer = tf.python_io.TFRecordWriter(
+      FLAGS.output_folder + '/dataset_' + set_name + str(j) + '.tf')
 
-  writer = tf.python_io.TFRecordWriter(
-      FLAGS.output_folder + '/dataset_' + set_name + '.tf')
+    query_doc_ids_path = (
+      FLAGS.output_folder + '/query_doc_ids_' + set_name + str(j) + '.txt')
+    with open(query_doc_ids_path, 'w') as ids_file:
+      for i, (query, doc_ids_docs) in enumerate(queries_docs.items()):
+        doc_ids, docs, labels = zip(*doc_ids_docs)
+        query_id = query_ids[query]
 
-  query_doc_ids_path = (
-      FLAGS.output_folder + '/query_doc_ids_' + set_name + '.txt')
-  with open(query_doc_ids_path, 'w') as ids_file:
-    for i, (query, doc_ids_docs) in enumerate(queries_docs.items()):
-      doc_ids, docs, labels = zip(*doc_ids_docs)
-      query_id = query_ids[query]
-
-      write_to_tf_record(writer=writer,
+        write_to_tf_record(writer=writer,
                          tokenizer=tokenizer,
-                         query=query, 
-                         docs=docs, 
+                         query=query,
+                         docs=docs,
                          labels=labels,
                          ids_file=ids_file,
                          query_id=query_id,
                          doc_ids=doc_ids)
 
-      if i % 1000 == 0:
-        print('Writing {} set, query {} of {}'.format(
+        if i % 1000 == 0:
+          print('Writing {} set, query {} of {}'.format(
             set_name, i, len(queries_docs)))
-        time_passed = time.time() - start_time
-        hours_remaining = (
+          time_passed = time.time() - start_time
+          hours_remaining = (
             len(queries_docs) - i) * time_passed / (max(1.0, i) * 3600)
-        print('Estimated hours remaining to write the {} set: {}'.format(
+          print('Estimated hours remaining to write the {} set: {}'.format(
             set_name, hours_remaining))
-  writer.close()
+    writer.close()
 
 
 def convert_train_dataset(tokenizer):
@@ -210,7 +222,7 @@ def main():
   if not os.path.exists(FLAGS.output_folder):
     os.mkdir(FLAGS.output_folder)
 
-  convert_eval_dataset(set_name='eval', tokenizer=tokenizer, use_answer=True)
+  convert_eval_dataset(set_name='eval', tokenizer=tokenizer, use_answer=False)
   print('Done!')
 
 if __name__ == '__main__':
